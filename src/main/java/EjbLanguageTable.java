975ecb0a45c726dc0c9ca83cf10dac1eeeb85a79
PawelKoszalkowski <pawel.koszalkowski@outlook.com>
2016-09-12 14:01:54 +0200

    working listing language list not working course listing yet

---
+++ src/main/java/EjbLanguageTable.java
@@ -0,0 +1,26 @@
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * @author Benjamin Olsson
+ */
+
+@Stateless
+public class EjbLanguageTable implements Serializable{
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public List<LanguageTable> getAllLanguages() {
+        return entityManager.createNamedQuery("selectAllLanguages").getResultList();
+    }
+
+    public void addLanguage(LanguageClass language){
+        LanguageTable lanTab = new LanguageTable();
+        lanTab.setLanguage(language.getLanguage());
+        entityManager.persist(lanTab);
+
+    }
+}
