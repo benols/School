d8003f01b06701de5ee6aa21833fbf2d6ae8f115
PawelKoszalkowski <pawel.koszalkowski@outlook.com>
2016-09-23 11:41:14 +0200

    added semester table, added packages, added new files

---
+++ src/main/java/se/school/ejb/EjbSemesterTable.java
@@ -0,0 +1,30 @@
+package se.school.ejb;
+import se.school.jpa.SemesterTable;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+/**
+ * @author Benjamin Olsson
+ */
+@Stateless
+public class EjbSemesterTable {
+    @PersistenceContext
+    EntityManager entityManager;
+
+    public List<SemesterTable> getAllSemesters(String located){
+        List<SemesterTable> l;
+        if (located.equals(""))
+            l = entityManager.createNamedQuery("selectAllSemesters").getResultList();
+        else
+            l = entityManager.createNamedQuery("selectAllSemesters").getResultList();
+        for(Object p:l){
+            System.err.println(p);
+        }
+
+        return l;
+    }
+
+}
